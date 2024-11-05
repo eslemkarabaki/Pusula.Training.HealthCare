aed25f0bd96117303ae0d0548f699c20c4f3ee32
selcukshn <muselcuksahin@gmail.com>
2024-11-05 17:51:39 +0300

    Added Entity Mappings for Patient, Address, Country, City, District

--- src/Pusula.Training.HealthCare.Application/HealthCareApplicationAutoMapperProfile.cs
+++ src/Pusula.Training.HealthCare.Application/HealthCareApplicationAutoMapperProfile.cs
@@ -4,6 +4,10 @@ using Pusula.Training.HealthCare.Patients;
 using Pusula.Training.HealthCare.Protocols;
 using Pusula.Training.HealthCare.Shared;
 using System;
+using Pusula.Training.HealthCare.Addresses;
+using Pusula.Training.HealthCare.Cities;
+using Pusula.Training.HealthCare.Countries;
+using Pusula.Training.HealthCare.Districts;
 
 namespace Pusula.Training.HealthCare;
 
@@ -18,7 +22,18 @@ public class HealthCareApplicationAutoMapperProfile : Profile
         CreateMap<Patient, PatientDto>();
         CreateMap<Patient, PatientExcelDto>();
         CreateMap<PatientDto, PatientUpdateDto>();
-        CreateMap<Patient, LookupDto<Guid>>().ForMember(dest => dest.DisplayName, opt => opt.MapFrom(src => src.FirstName));
+        CreateMap<PatientWithNavigationProperties, PatientWithNavigationPropertiesDto>();
+        CreateMap<Patient, LookupDto<Guid>>()
+            .ForMember(dest => dest.DisplayName, opt => opt.MapFrom(src => src.FirstName));
+
+        CreateMap<Address, AddressDto>();
+        CreateMap<AddressWithNavigationProperties, AddressWithNavigationPropertiesDto>();
+
+        CreateMap<Country, CountryDto>();
+
+        CreateMap<City, CityDto>();
+
+        CreateMap<District, DistrictDto>();
 
         CreateMap<Protocol, ProtocolDto>();
         CreateMap<Protocol, ProtocolExcelDto>();
@@ -28,6 +43,7 @@ public class HealthCareApplicationAutoMapperProfile : Profile
         CreateMap<Department, DepartmentDto>();
         CreateMap<Department, DepartmentExcelDto>();
         CreateMap<DepartmentDto, DepartmentUpdateDto>();
-        CreateMap<Department, LookupDto<Guid>>().ForMember(dest => dest.DisplayName, opt => opt.MapFrom(src => src.Name));
+        CreateMap<Department, LookupDto<Guid>>()
+            .ForMember(dest => dest.DisplayName, opt => opt.MapFrom(src => src.Name));
     }
 }
