4c99d43ea42b118faa94c32bac58f6268fb05c99
selcukshn <muselcuksahin@gmail.com>
2024-11-09 16:43:34 +0300

    Updated Address models in preparation for Patient Registration

--- src/Pusula.Training.HealthCare.Application.Contracts/Addresses/AddressDto.cs
+++ src/Pusula.Training.HealthCare.Application.Contracts/Addresses/AddressDto.cs
@@ -7,9 +7,16 @@ namespace Pusula.Training.HealthCare.Addresses;
 public class AddressDto : AuditedEntityDto<Guid>, IHasConcurrencyStamp
 {
     public Guid PatientId { get; set; }
+
+    public Guid CountryId { get; set; }
     public string Country { get; set; } = null!;
+
+    public Guid CityId { get; set; }
     public string City { get; set; } = null!;
+
+    public Guid DistrictId { get; set; }
     public string District { get; set; } = null!;
+
     public string AddressLine { get; set; } = null!;
 
     public string ConcurrencyStamp { get; set; } = null!;
--- src/Pusula.Training.HealthCare.EntityFrameworkCore/Patients/EfCorePatientRepository.cs
+++ src/Pusula.Training.HealthCare.EntityFrameworkCore/Patients/EfCorePatientRepository.cs
@@ -45,11 +45,12 @@ public class EfCorePatientRepository(IDbContextProvider<HealthCareDbContext> dbC
         CancellationToken cancellationToken = default)
     {
         return await ApplyFilter(await GetQueryableAsync(), filterText, firstName, lastName, birthDateMin, birthDateMax,
-                identityNumber,
-                emailAddress, mobilePhoneNumber, homePhoneNumber, gender, bloodType, maritalStatus, countryId)
-            .OrderBy(string.IsNullOrWhiteSpace(sorting) ? PatientConsts.GetDefaultSorting(false) : sorting)
-            .PageBy(skipCount, maxResultCount)
-            .ToListAsync(cancellationToken);
+                                 identityNumber,
+                                 emailAddress, mobilePhoneNumber, homePhoneNumber, gender, bloodType, maritalStatus,
+                                 countryId)
+                     .OrderBy(string.IsNullOrWhiteSpace(sorting) ? PatientConsts.GetDefaultSorting(false) : sorting)
+                     .PageBy(skipCount, maxResultCount)
+                     .ToListAsync(cancellationToken);
     }
 
     public async Task<List<PatientWithAddressAndCountry>> GetListWithAddressAndCountryAsync(string? sorting = null,
@@ -57,9 +58,9 @@ public class EfCorePatientRepository(IDbContextProvider<HealthCareDbContext> dbC
         CancellationToken cancellationToken = default)
     {
         return await (await GetQueryForAddressAndCountryAsync())
-            .OrderBy(string.IsNullOrWhiteSpace(sorting) ? PatientConsts.GetDefaultSorting(false) : sorting)
-            .PageBy(skipCount, maxResultCount)
-            .ToListAsync(cancellationToken);
+                     .OrderBy(string.IsNullOrWhiteSpace(sorting) ? PatientConsts.GetDefaultSorting(false) : sorting)
+                     .PageBy(skipCount, maxResultCount)
+                     .ToListAsync(cancellationToken);
     }
 
     public virtual async Task<long> GetCountAsync(
@@ -79,61 +80,67 @@ public class EfCorePatientRepository(IDbContextProvider<HealthCareDbContext> dbC
         CancellationToken cancellationToken = default)
     {
         return await ApplyFilter(await GetDbSetAsync(), filterText, firstName, lastName, birthDateMin, birthDateMax,
-                identityNumber, emailAddress, mobilePhoneNumber, homePhoneNumber, gender, bloodType, maritalStatus,
-                countryId)
+                                 identityNumber, emailAddress, mobilePhoneNumber, homePhoneNumber, gender, bloodType,
+                                 maritalStatus,
+                                 countryId)
             .LongCountAsync(GetCancellationToken(cancellationToken));
     }
 
     protected virtual async Task<IQueryable<PatientWithAddressAndCountry>> GetQueryForAddressAndCountryAsync()
     {
         return from patient in await GetDbSetAsync()
-            join patient_country in (await GetDbContextAsync()).Set<Country>() on patient.CountryId equals
-                patient_country.Id into
-                patient_countries
-            from patient_country in patient_countries.DefaultIfEmpty()
-            join address in (await GetDbContextAsync()).Set<Address>() on patient.Id equals address.PatientId into
-                addresses
-            from address in addresses.DefaultIfEmpty()
-            join district in (await GetDbContextAsync()).Set<District>() on address.DistrictId equals district.Id into
-                districts
-            from district in districts.DefaultIfEmpty()
-            join city in (await GetDbContextAsync()).Set<City>() on district.CityId equals city.Id into cities
-            from city in cities.DefaultIfEmpty()
-            join address_country in (await GetDbContextAsync()).Set<Country>() on city.CountryId equals address_country
-                .Id into address_countries
-            from address_country in address_countries.DefaultIfEmpty()
-            select new PatientWithAddressAndCountry
-            {
-                Id = patient.Id,
-                EmailAddress = patient.EmailAddress,
-                FirstName = patient.FirstName,
-                IdentityNumber = patient.IdentityNumber,
-                HomePhoneNumber = patient.HomePhoneNumber,
-                BirthDate = patient.BirthDate,
-                Gender = patient.Gender,
-                BloodType = patient.BloodType,
-                LastName = patient.LastName,
-                MaritalStatus = patient.MaritalStatus,
-                MobilePhoneNumber = patient.MobilePhoneNumber,
-                CreationTime = patient.CreationTime,
-                Country = patient_country.Name,
-                CountryId = patient_country.Id,
-                Address = address != null
-                    ? new AddressWithRelations()
-                    {
-                        Id = address.Id,
-                        PatientId = patient.Id,
-                        Country = address_country.Name,
-                        City = city.Name,
-                        District = district.Name,
-                        AddressLine = address.AddressLine,
-                        CreationTime = address.CreationTime
-                    }
-                    : null
-            };
+               join patient_country in (await GetDbContextAsync()).Set<Country>() on patient.CountryId equals
+                   patient_country.Id into
+                   patient_countries
+               from patient_country in patient_countries.DefaultIfEmpty()
+               join address in (await GetDbContextAsync()).Set<Address>() on patient.Id equals address.PatientId into
+                   addresses
+               from address in addresses.DefaultIfEmpty()
+               join district in (await GetDbContextAsync()).Set<District>() on address.DistrictId equals district.Id
+                   into
+                   districts
+               from district in districts.DefaultIfEmpty()
+               join city in (await GetDbContextAsync()).Set<City>() on district.CityId equals city.Id into cities
+               from city in cities.DefaultIfEmpty()
+               join address_country in (await GetDbContextAsync()).Set<Country>() on city.CountryId equals
+                   address_country
+                       .Id into address_countries
+               from address_country in address_countries.DefaultIfEmpty()
+               select new PatientWithAddressAndCountry
+               {
+                   Id = patient.Id,
+                   EmailAddress = patient.EmailAddress,
+                   FirstName = patient.FirstName,
+                   IdentityNumber = patient.IdentityNumber,
+                   HomePhoneNumber = patient.HomePhoneNumber,
+                   BirthDate = patient.BirthDate,
+                   Gender = patient.Gender,
+                   BloodType = patient.BloodType,
+                   LastName = patient.LastName,
+                   MaritalStatus = patient.MaritalStatus,
+                   MobilePhoneNumber = patient.MobilePhoneNumber,
+                   CreationTime = patient.CreationTime,
+                   Country = patient_country.Name,
+                   CountryId = patient_country.Id,
+                   Address = address != null
+                       ? new AddressWithRelations()
+                       {
+                           Id = address.Id,
+                           PatientId = patient.Id,
+                           DistrictId = address.DistrictId,
+                           District = district.Name,
+                           CountryId = address_country.Id,
+                           Country = address_country.Name,
+                           CityId = city.Id,
+                           City = city.Name,
+                           AddressLine = address.AddressLine,
+                           CreationTime = address.CreationTime
+                       }
+                       : null
+               };
     }
 
-    #region Delete
+#region Delete
 
     public virtual async Task DeleteAllAsync(
         string? filterText = null,
@@ -152,15 +159,16 @@ public class EfCorePatientRepository(IDbContextProvider<HealthCareDbContext> dbC
         CancellationToken cancellationToken = default)
     {
         var ids = ApplyFilter(await GetQueryableAsync(), filterText, firstName, lastName, birthDateMin, birthDateMax,
-                identityNumber,
-                emailAddress, mobilePhoneNumber, homePhoneNumber, gender, bloodType, maritalStatus, countryId)
+                              identityNumber,
+                              emailAddress, mobilePhoneNumber, homePhoneNumber, gender, bloodType, maritalStatus,
+                              countryId)
             .Select(e => e.Id);
         await DeleteManyAsync(ids, cancellationToken: GetCancellationToken(cancellationToken));
     }
 
-    #endregion
+#endregion
 
-    #region Apply Filter
+#region Apply Filter
 
     protected virtual IQueryable<Patient> ApplyFilter(
         IQueryable<Patient> query,
@@ -179,24 +187,24 @@ public class EfCorePatientRepository(IDbContextProvider<HealthCareDbContext> dbC
         Guid? countryId = null)
     {
         return query
-            .WhereIf(!string.IsNullOrWhiteSpace(filterText),
-                e => e.FirstName!.Contains(filterText!) || e.LastName!.Contains(filterText!) ||
-                     e.IdentityNumber!.Contains(filterText!) || e.EmailAddress!.Contains(filterText!) ||
-                     e.MobilePhoneNumber!.Contains(filterText!) || e.HomePhoneNumber!.Contains(filterText!))
-            .WhereIf(!string.IsNullOrWhiteSpace(firstName), e => e.FirstName.Contains(firstName!))
-            .WhereIf(!string.IsNullOrWhiteSpace(lastName), e => e.LastName.Contains(lastName!))
-            .WhereIf(birthDateMin.HasValue, e => e.BirthDate >= birthDateMin!.Value)
-            .WhereIf(birthDateMax.HasValue, e => e.BirthDate <= birthDateMax!.Value)
-            .WhereIf(!string.IsNullOrWhiteSpace(identityNumber), e => e.IdentityNumber.Contains(identityNumber!))
-            .WhereIf(!string.IsNullOrWhiteSpace(emailAddress), e => e.EmailAddress.Contains(emailAddress!))
-            .WhereIf(!string.IsNullOrWhiteSpace(mobilePhoneNumber),
-                e => e.MobilePhoneNumber.Contains(mobilePhoneNumber!))
-            .WhereIf(!string.IsNullOrWhiteSpace(homePhoneNumber),
-                e => e.HomePhoneNumber != null && e.HomePhoneNumber.Contains(homePhoneNumber!))
-            .WhereIf(gender.HasValue, e => e.Gender == gender!.Value)
-            .WhereIf(bloodType.HasValue, e => e.BloodType == bloodType!.Value)
-            .WhereIf(maritalStatus.HasValue, e => e.MaritalStatus == maritalStatus!.Value)
-            .WhereIf(countryId.HasValue, e => e.CountryId == countryId!.Value);
+               .WhereIf(!string.IsNullOrWhiteSpace(filterText),
+                        e => e.FirstName!.Contains(filterText!) || e.LastName!.Contains(filterText!) ||
+                            e.IdentityNumber!.Contains(filterText!) || e.EmailAddress!.Contains(filterText!) ||
+                            e.MobilePhoneNumber!.Contains(filterText!) || e.HomePhoneNumber!.Contains(filterText!))
+               .WhereIf(!string.IsNullOrWhiteSpace(firstName), e => e.FirstName.Contains(firstName!))
+               .WhereIf(!string.IsNullOrWhiteSpace(lastName), e => e.LastName.Contains(lastName!))
+               .WhereIf(birthDateMin.HasValue, e => e.BirthDate >= birthDateMin!.Value)
+               .WhereIf(birthDateMax.HasValue, e => e.BirthDate <= birthDateMax!.Value)
+               .WhereIf(!string.IsNullOrWhiteSpace(identityNumber), e => e.IdentityNumber.Contains(identityNumber!))
+               .WhereIf(!string.IsNullOrWhiteSpace(emailAddress), e => e.EmailAddress.Contains(emailAddress!))
+               .WhereIf(!string.IsNullOrWhiteSpace(mobilePhoneNumber),
+                        e => e.MobilePhoneNumber.Contains(mobilePhoneNumber!))
+               .WhereIf(!string.IsNullOrWhiteSpace(homePhoneNumber),
+                        e => e.HomePhoneNumber != null && e.HomePhoneNumber.Contains(homePhoneNumber!))
+               .WhereIf(gender.HasValue, e => e.Gender == gender!.Value)
+               .WhereIf(bloodType.HasValue, e => e.BloodType == bloodType!.Value)
+               .WhereIf(maritalStatus.HasValue, e => e.MaritalStatus == maritalStatus!.Value)
+               .WhereIf(countryId.HasValue, e => e.CountryId == countryId!.Value);
     }
 
     // protected virtual IQueryable<PatientWithNavigationProperties> ApplyFilter(
@@ -238,5 +246,5 @@ public class EfCorePatientRepository(IDbContextProvider<HealthCareDbContext> dbC
     //         .WhereIf(countryId.HasValue, e => e.Country.Id == countryId!.Value);
     // }
 
-    #endregion
+#endregion
 }
