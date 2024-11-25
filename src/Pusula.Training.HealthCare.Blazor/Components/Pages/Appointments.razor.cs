29e20988fe42cc0bd603195385eb401c50901dc4
selcukshn <muselcuksahin@gmail.com>
2024-11-25 21:47:07 +0300

    appointment fix

--- src/Pusula.Training.HealthCare.Blazor/Components/Pages/Appointments.razor.cs
+++ src/Pusula.Training.HealthCare.Blazor/Components/Pages/Appointments.razor.cs
@@ -25,49 +25,56 @@ public partial class Appointments
     private List<DoctorDto> Doctors { get; set; } = [];
     private List<DepartmentDto> Departments { get; set; } = [];
     private List<AppointmentTypeDto> AppointmentTypes { get; set; } = [];
-    private Guid SelectedPatientId { get; set; } 
+    private Guid SelectedPatientId { get; set; }
     private Guid SelectedDepartmentId { get; set; }
     private Guid SelectedDoctorId { get; set; }
     private string PatientFilter { get; set; } = string.Empty;
     private string ErrorMessage { get; set; } = string.Empty;
     private IReadOnlyList<PatientDto> Patients { get; set; } = [];
     private SfAutoComplete<Guid, PatientDto> refAutoComplate { get; set; }
-    private bool valueSelected => SelectedPatientId != Guid.Empty && SelectedDepartmentId != Guid.Empty && SelectedDoctorId != Guid.Empty;
 
-    
+    private bool valueSelected =>
+        SelectedPatientId != Guid.Empty && SelectedDepartmentId != Guid.Empty && SelectedDoctorId != Guid.Empty;
+
+    private GetPatientsInputValidator PatientsInputValidator { get; set; } = new();
+
     protected async void GetPatientFilter(FilteringEventArgs args)
     {
         args.PreventDefaultAction = true;
-        if (args.Text.IsNullOrEmpty())
+        var filter = new GetPatientsInput { FilterText = args.Text };
+        var result = await PatientsInputValidator.ValidateAsync(filter);
+        if (!result.IsValid)
         {
-            return;            
+            return;
         }
 
-        var patients = await PatientAppService.GetListAsync(new GetPatientsInput { FilterText=args.Text});
+        var patients = await PatientAppService.GetListAsync(filter);
         Patients = patients.Items;
 
         refAutoComplate.DataSource = Patients;
-        var query = new Query().Where(new WhereFilter() { Field = "FullName", Operator = "contains", value = args.Text, IgnoreCase = true });
+        var query = new Query().Where(
+            new WhereFilter()
+            {
+                Field = "FullName",
+                Operator = "contains",
+                value = args.Text,
+                IgnoreCase = true
+            }
+        );
         query = !string.IsNullOrEmpty(args.Text) ? query : new Query();
         await refAutoComplate.FilterAsync(Patients, query);
         await InvokeAsync(StateHasChanged);
-
     }
 
-    private void NavigateToPage()
-    {
-        NavigationManager.NavigateTo("/patients"); 
-    }
+    private void NavigateToPage() => NavigationManager.NavigateTo("/patients");
 
     protected override async Task OnInitializedAsync()
-    {        
+    {
         Departments = await DepartmentsAppService.GetListDepartmentsAsync();
         AppointmentTypes = await AppointmentTypeAppService.GetListAppointmentTypesAsync();
-        
-        
     }
 
-    protected async void DepartmentSelect(ChangeEventArgs<Guid, DepartmentDto>args)
+    protected async void DepartmentSelect(ChangeEventArgs<Guid, DepartmentDto> args)
     {
         SelectedDepartmentId = args.ItemData.Id;
         Doctors = await DoctorsAppService.GetListDoctorsAsync(args.ItemData.Id);
@@ -79,22 +86,20 @@ public partial class Appointments
         SelectedDoctorId = args.ItemData.Id;
         AppointmentLists = await AppointmentsAppService.GetListAppointmentsAsync(args.ItemData.Id);
         await InvokeAsync(StateHasChanged);
-    }    
+    }
 
     public async void OnActionBegin(ActionEventArgs<AppointmentDto> args)
     {
-        if (args.ActionType == Syncfusion.Blazor.Schedule.ActionType.EventCreate) {
-
-            var input=ObjectMapper.Map<AppointmentDto, AppointmentCreateDto>(args.AddedRecords.First());
+        if (args.ActionType == ActionType.EventCreate)
+        {
+            var input = ObjectMapper.Map<AppointmentDto, AppointmentCreateDto>(args.AddedRecords.First());
             input.DepartmentId = SelectedDepartmentId;
             input.DoctorId = SelectedDoctorId;
             input.PatientId = SelectedPatientId;
             await AppointmentsAppService.CreateAsync(input);
             AppointmentLists = await AppointmentsAppService.GetListAppointmentsAsync(SelectedDoctorId);
             await InvokeAsync(StateHasChanged);
-        }
-
-        else if(args.ActionType == Syncfusion.Blazor.Schedule.ActionType.EventChange)
+        } else if (args.ActionType == ActionType.EventChange)
         {
             var x = args.ChangedRecords.First();
             var input = ObjectMapper.Map<AppointmentDto, AppointmentUpdateDto>(x);
@@ -104,17 +109,12 @@ public partial class Appointments
             await AppointmentsAppService.UpdateAsync(x.Id, input);
             AppointmentLists = await AppointmentsAppService.GetListAppointmentsAsync(SelectedDoctorId);
             await InvokeAsync(StateHasChanged);
-        }
-        else if(args.ActionType == Syncfusion.Blazor.Schedule.ActionType.EventRemove)
+        } else if (args.ActionType == ActionType.EventRemove)
         {
-            var x = args.DeletedRecords.First();            
+            var x = args.DeletedRecords.First();
             await AppointmentsAppService.DeleteAsync(x.Id);
             AppointmentLists = await AppointmentsAppService.GetListAppointmentsAsync(SelectedDoctorId);
             await InvokeAsync(StateHasChanged);
         }
     }
-
-   
-
-
 }
