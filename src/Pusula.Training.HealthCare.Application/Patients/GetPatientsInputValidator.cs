558ad2bb0ff48c269e48278aed16e6cdcf8197e0
selcukshn <muselcuksahin@gmail.com>
2024-11-29 12:38:58 +0300

    GetPatientInputValidator updated

--- src/Pusula.Training.HealthCare.Application/Patients/GetPatientsInputValidator.cs
+++ src/Pusula.Training.HealthCare.Application/Patients/GetPatientsInputValidator.cs
@@ -8,23 +8,32 @@ public class GetPatientsInputValidator : AbstractValidator<GetPatientsInput>
     public GetPatientsInputValidator()
     {
         RuleFor(e => e.FilterText)
-            .MinimumLength(3)
+            .Length(3,128)
             .When(e => !e.FilterText.IsNullOrWhiteSpace())
-            .WithMessage("The filter text must be at least {minimumLength} characters long.");
+            .WithMessage("The filter text must be between {minLength} and {maxLength} characters.");
+        
+        RuleFor(e => e.No)
+            .GreaterThan(0)
+            .LessThan(int.MaxValue);
+
+        RuleFor(e => e.FullName)
+            .Length(3,PatientConsts.FirstNameMaxLength + PatientConsts.LastNameMaxLength)
+            .When(e => !e.FullName.IsNullOrWhiteSpace())
+            .WithMessage("The name must be between {minLength} and {maxLength} characters.");
 
         RuleFor(e => e.PassportNumber)
-            .MinimumLength(3)
+            .Length(3,PatientConsts.PassportNumberMaxLength)
             .When(e => !e.PassportNumber.IsNullOrWhiteSpace())
-            .WithMessage("The passport number must be at least {minimumLength} characters long.");
+            .WithMessage("The passport number must be between {minLength} and {maxLength} characters.");
 
         RuleFor(e => e.IdentityNumber)
-            .MinimumLength(3)
+            .Length(3, PatientConsts.IdentityNumberMaxLength)
             .When(e => !e.IdentityNumber.IsNullOrWhiteSpace())
-            .WithMessage("The passport number must be at least {minimumLength} characters long.");
+            .WithMessage("The identity number be between {minLength} and {maxLength} characters.");
 
         RuleFor(e => e.MobilePhoneNumber)
-            .MinimumLength(3)
+            .Length(3, PatientConsts.PhoneNumberMaxLength)
             .When(e => !e.MobilePhoneNumber.IsNullOrWhiteSpace())
-            .WithMessage("The passport number must be at least {minimumLength} characters long.");
+            .WithMessage("The mobile phone number must be between {minLength} and {maxLength} characters.");
     }
 }
