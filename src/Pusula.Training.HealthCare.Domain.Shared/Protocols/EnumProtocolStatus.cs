1008ba80cef249f02d00eec69642321e274417a9
selcukshn <muselcuksahin@gmail.com>
2024-12-02 08:44:42 +0300

    Edited Protocol in core layers

--- src/Pusula.Training.HealthCare.Domain.Shared/Protocols/EnumProtocolStatus.cs
+++ src/Pusula.Training.HealthCare.Domain.Shared/Protocols/EnumProtocolStatus.cs
@@ -1,14 +1,3 @@
 namespace Pusula.Training.HealthCare.Protocols;
 
-public enum EnumProtocolStatus
-{
-    Open = 1,
-    Closed = 2,
-    Pending = 3,
-    Cancelled = 4,
-    InProgress = 5,
-    Referred = 6,
-    AwaitingApproval = 7,
-    Completed = 8,
-    OnHold = 9
-}
+public enum EnumProtocolStatus { InProgress = 1, Completed = 2, Closed = 3, Cancelled = 4 }
--- src/Pusula.Training.HealthCare.EntityFrameworkCore/Protocols/EfCoreProtocolRepository.cs
+++ src/Pusula.Training.HealthCare.EntityFrameworkCore/Protocols/EfCoreProtocolRepository.cs
@@ -16,4 +16,90 @@ namespace Pusula.Training.HealthCare.Protocols;
 public class EfCoreProtocolRepository(IDbContextProvider<HealthCareDbContext> dbContextProvider)
     : EfCoreRepository<HealthCareDbContext, Protocol, Guid>(dbContextProvider), IProtocolRepository
 {
+    public async Task<List<Protocol>> GetListAsync(
+        Guid? patientId = null,
+        Guid? doctorId = null,
+        Guid? departmentId = null,
+        Guid? protocolTypeId = null,
+        EnumProtocolStatus? status = null,
+        DateTime? startTime = null,
+        DateTime? endTime = null,
+        string? sorting = null,
+        int maxResultCount = int.MaxValue,
+        int skipCount = 0,
+        CancellationToken cancellationToken = default
+    ) =>
+        await ApplyFilter(
+                  await GetQueryableAsync(), string.Empty, patientId, doctorId, departmentId, protocolTypeId, status,
+                  startTime, endTime
+              )
+              .OrderBy(GetSorting(sorting, true))
+              .Skip(skipCount)
+              .Take(maxResultCount)
+              .ToListAsync(GetCancellationToken(cancellationToken));
+
+    public async Task<List<Protocol>> GetListWithDetailsAsync(
+        Guid? patientId = null,
+        Guid? doctorId = null,
+        Guid? departmentId = null,
+        Guid? protocolTypeId = null,
+        EnumProtocolStatus? status = null,
+        DateTime? startTime = null,
+        DateTime? endTime = null,
+        string? sorting = null,
+        int maxResultCount = int.MaxValue,
+        int skipCount = 0,
+        CancellationToken cancellationToken = default
+    ) =>
+        await ApplyFilter(
+                  await WithDetailsAsync(e => e.Patient, e => e.Department, e => e.Doctor, e => e.ProtocolType),
+                  string.Empty, patientId, doctorId, departmentId, protocolTypeId, status,
+                  startTime, endTime
+              )
+              .OrderBy(GetSorting(sorting, false))
+              .Skip(skipCount)
+              .Take(maxResultCount)
+              .ToListAsync(GetCancellationToken(cancellationToken));
+
+    public async Task<long> GetCountAsync(
+        Guid? patientId = null,
+        Guid? doctorId = null,
+        Guid? departmentId = null,
+        Guid? protocolTypeId = null,
+        EnumProtocolStatus? status = null,
+        DateTime? startTime = null,
+        DateTime? endTime = null,
+        CancellationToken cancellationToken = default
+    ) =>
+        await ApplyFilter(
+                await GetQueryableAsync(),
+                string.Empty, patientId, doctorId, departmentId, protocolTypeId, status,
+                startTime, endTime
+            )
+            .LongCountAsync(GetCancellationToken(cancellationToken));
+
+    protected virtual IQueryable<Protocol> ApplyFilter(
+        IQueryable<Protocol> query,
+        string? filterText = null, // todo
+        Guid? patientId = null,
+        Guid? doctorId = null,
+        Guid? departmentId = null,
+        Guid? protocolTypeId = null,
+        EnumProtocolStatus? status = null,
+        DateTime? startTime = null,
+        DateTime? endTime = null
+    ) =>
+        query
+            .WhereIf(patientId.HasValue, e => e.PatientId == patientId!.Value)
+            .WhereIf(doctorId.HasValue, e => e.DoctorId == doctorId!.Value)
+            .WhereIf(departmentId.HasValue, e => e.DepartmentId == departmentId!.Value)
+            .WhereIf(protocolTypeId.HasValue, e => e.ProtocolTypeId == protocolTypeId!.Value)
+            .WhereIf(status.HasValue, e => e.Status == status!.Value)
+            .WhereIf(startTime.HasValue, e => e.StartTime >= startTime!.Value)
+            .WhereIf(endTime.HasValue, e => !e.EndTime.HasValue || e.EndTime <= endTime!.Value);
+
+    protected virtual string GetSorting(string? sorting, bool withEntityName) =>
+        sorting.IsNullOrWhiteSpace() ?
+            ProtocolConsts.GetDefaultSorting(withEntityName) :
+            $"{(withEntityName ? "Protocol." : string.Empty)}{sorting}";
 }
