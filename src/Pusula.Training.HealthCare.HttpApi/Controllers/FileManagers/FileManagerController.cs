612c406cad18d6cc5b110bfe296a7c84c5b9166c
selcukshn <muselcuksahin@gmail.com>
2024-12-02 09:32:10 +0300

    Created FileManager controller for FileManager component #For later

---
+++ src/Pusula.Training.HealthCare.HttpApi/Controllers/FileManagers/FileManagerController.cs
@@ -0,0 +1,12 @@
+using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pusula.Training.HealthCare.Controllers.FileManagers;
+
+[Area("app")]
+[ControllerName("FileManager")]
+[Route("api/app/file-manager")]
+public class FileManagerController : HealthCareController
+{
+    
+}
