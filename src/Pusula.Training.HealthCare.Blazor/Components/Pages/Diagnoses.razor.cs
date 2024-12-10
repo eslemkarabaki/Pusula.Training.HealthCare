eceb17a146abd3e3b83fe944a8c5ee05dab48e2d
selcukshn <muselcuksahin@gmail.com>
2024-12-10 12:14:31 +0300

    migrations updated

--- src/Pusula.Training.HealthCare.Blazor/Components/Pages/Diagnoses.razor.cs
+++ src/Pusula.Training.HealthCare.Blazor/Components/Pages/Diagnoses.razor.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
-using Prism.Dialogs;
 using Pusula.Training.HealthCare.Diagnoses;
 using Syncfusion.Blazor.Popups;
 
