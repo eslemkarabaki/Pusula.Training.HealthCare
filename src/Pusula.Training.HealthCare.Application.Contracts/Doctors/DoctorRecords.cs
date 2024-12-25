932e2f817f70f5d298c5f14fc2bae49907809da1
selcukshn <muselcuksahin@gmail.com>
2024-12-25 08:14:36 +0300

    Signal-r for created protocol notifications to send doctor

--- src/Pusula.Training.HealthCare.Application.Contracts/Doctors/DoctorRecords.cs
+++ src/Pusula.Training.HealthCare.Application.Contracts/Doctors/DoctorRecords.cs
@@ -3,3 +3,5 @@ using System;
 namespace Pusula.Training.HealthCare.Doctors;
 
 public record GetDoctorInput(Guid? DoctorId = null, Guid? UserId = null);
+
+public record SendDoctorNotificationInput(Guid DoctorId, string Message);
--- src/Pusula.Training.HealthCare.HttpApi/Hub/NotificationHub.cs
+++ src/Pusula.Training.HealthCare.HttpApi/Hub/NotificationHub.cs
@@ -2,63 +2,22 @@
 using Pusula.Training.HealthCare.Permissions;
 using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Pusula.Training.HealthCare.Doctors;
 using Volo.Abp.AspNetCore.SignalR;
-using Volo.Abp.Users;
 
-namespace Pusula.Training.HealthCare.Hub
+namespace Pusula.Training.HealthCare.Hub;
+
+[HubRoute("hub/notification")]
+public class NotificationHub : AbpHub
 {
-    public class NotificationHub : AbpHub
-    {
-        private readonly ICurrentUser _currentUser;
+    public async Task JoinGroup(string groupName) => await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
 
-        public NotificationHub(ICurrentUser currentUser)
-        {
-            _currentUser = currentUser;
-        }
-         
-        public async Task JoinGroup()
-        {
-            var userId = Context.UserIdentifier;
-            if (_currentUser.IsInRole(HealthCareRoles.Doctor))
-            {
-                await Groups.AddToGroupAsync(Context.ConnectionId, _currentUser.Id.ToString());
-                var a = _currentUser.Id.ToString();
+    public async Task SendNotificationToDoctor(Guid doctorId, string message) =>
+        await Clients.Group(doctorId.ToString()).SendAsync("ReceiveNotification", message);
 
-            }
-            else if (_currentUser.IsInRole(HealthCareRoles.RadyologyTechnician))
-            {
-                await Groups.AddToGroupAsync(Context.ConnectionId, HealthCareRoles.RadyologyTechnician);
-            }
-            else
-            { 
-                Console.WriteLine($"User role does not match the provided roles {HealthCareRoles.Doctor} or {HealthCareRoles.RadyologyTechnician}");
-            } 
-        }
-        public async Task SendNotificationToDoctor(string message, Guid doctorId)
-        {
-            try
-            {
-                var a = doctorId.ToString();
+    public async Task SendNotificationToRadiologyTechnicians(string message) =>
+        await Clients.Group(HealthCareRoles.RadyologyTechnician).SendAsync("ReceiveNotification", message);
 
-                await Clients.Group(doctorId.ToString()).SendAsync("ReceiveNotification", message);
-
-                Console.WriteLine("Notification sent successfully.");
-            }
-            catch (Exception ex)
-            {
-                Console.Error.WriteLine($"Error sending notification to doctor: {ex.Message}");
-                await Clients.Caller.SendAsync("ReceiveNotification", "Failed to send notification to doctor.");
-            }
-        }
-
-        public async Task SendNotificationToRadiologyTechnicians(string message)
-        { 
-            await Clients.Group(HealthCareRoles.RadyologyTechnician).SendAsync("ReceiveNotification", message);
-        }
-
-        public async Task GetMessage(string message)
-        {
-            Console.WriteLine("Message Received: " + message);
-        }
-    }
+    public async Task GetMessage(string message) => Console.WriteLine("Message Received: " + message);
 }
